7e8ba9772dad58b3f9403c83fbb736dd6030bd5b
Riripii <85088379+Riripii@users.noreply.github.com>
2021-08-20 13:08:56 +0700

    GUEST

--- start.bat
+++ start.bat
@@ -3,19 +3,19 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server by Farhan & amelia" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator Free123 /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net user Guest FarhanID /add >nul
+net localgroup administrators Guest /add >nul
+net user Guest /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant Guest:F >nul
+ICACLS C:\Windows\installer /grant Guest:F >nul
 echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
-echo Username: administrator
-echo Password: Free123
-echo You can login now.
+echo USERNAME : Guest
+echo PASSWORD : FarhanID
+echo LOGIN DI APK RDP
 ping -n 10 127.0.0.1 >nul
